372386a8ad4d33a996df394cb6a3c6dafd2de8bd
julian <juliandecastelli@gmail.com>
2022-08-25 16:59:04 -0300

    upgrade code to take parameters for multiple request and concat to the .json file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 import fetch from "node-fetch";
-const request = "https://api.spoonacular.com/recipes/random?number=100&apiKey="; //your key;
+const request = "https://api.spoonacular.com/recipes/random?number=1&apiKey="; //your key;
 
 const fs = require("fs");
 async function makeRequest(url) {
@@ -11,15 +11,19 @@ async function makeRequest(url) {
       return data;
     });
 }
-async function proceede() {
+async function proceede(i) {
   const data = await makeRequest(request);
+  for (i; i > 0; i--) {
+    let item = await makeRequest(request);
+    data.recipes.push(item.recipes[0]);
+  }
   return data;
 }
-async function write() {
-  const json = JSON.stringify(await proceede());
+async function write(i) {
+  const json = JSON.stringify(await proceede(i));
   fs.writeFile("recipeData.json", json, (err) => {
     if (err) throw err;
     console.log("Data written to file");
   });
 }
-write();
+write(3);
